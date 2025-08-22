dbeeeed4c7dfde0f187a47ada851c1d28f5a3482
minyhacktelegram <you@example.com>
2025-08-22 05:40:19 -0700

    update

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ bot = TeleBot(API_TOKEN)
 
 key = InlineKeyboardMarkup(row_width=2)
 btn1 = InlineKeyboardButton("🦊 روباه رندوم", callback_data="get_fox")
-key.add(btn1)
+btn2 = InlineKeyboardButton("سگ رندوم 🐶", callback_data="get_dogs")
+
+key.add(btn1,btn2)
 
 @bot.message_handler(commands=["start"])
 def start_bot(message):
 
 
-    bot.send_message(chat_id= message.chat.id , text="به ربات ما خوش آمدید\nبرای دریافت عکس روباه از دکمه پایین استفاده کنید",reply_markup=key)
+    bot.send_message(chat_id= message.chat.id , text="به ربات ما خوش آمدید\nبرای دریافت عکس روباه و عکس سگ از دکمه پایین استفاده کنید",reply_markup=key)
 
 @bot.callback_query_handler(func= lambda call:True)
 def api_fox(call):
@@ -24,7 +26,12 @@ def api_fox(call):
         get_image = url_fox.json()
         image_url = get_image["image"]
         bot.send_photo(chat_id=call.message.chat.id , photo=image_url, caption="🦊 روباه رندوم", reply_markup=key)
-
+    
+    elif call.data == "get_dogs":
+        url_fox = get("https://dog.ceo/api/breeds/image/random")
+        get_image = url_fox.json()
+        image_url = get_image["message"]
+        bot.send_photo(chat_id=call.message.chat.id , photo=image_url, caption="سگ رندوم 🐶", reply_markup=key)
 
     else:
         bot.send_message(chat_id= call.message.chat.id , text="پیغام شما برای من تعریف نشده", reply_markup=key)
